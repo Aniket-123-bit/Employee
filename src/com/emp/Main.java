14d12e758252176786d15b924d7530dd8d0c9b17
Aniket-123-bit <anisunny782@gmail.com>
2021-05-18 15:21:54 +0530

    commit multipleCompany push

---
+++ src/com/emp/Main.java
@@ -0,0 +1,57 @@
+package com.emp;
+
+public class Main {
+
+        static void calculateTotalwage(String company, int empRatePerHr, int numOfWorkingDays, int maxHoursPerMonth) {
+            int partTimeEmpHr=4;
+            int fullTimeEmpHrs = 8;
+            int empWage=0,totalMonthlyWage=0;
+            int totalWorkingHrs=0,totalWorkingDays=0;
+            while (totalWorkingDays < numOfWorkingDays && totalWorkingHrs < maxHoursPerMonth) {
+                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+                switch (empCheck) {
+
+                    case 0:
+                        System.out.println("Employee is absent");
+                        break;
+
+                    case 1:
+                        System.out.println("Employee is Present");
+                        empWage = Math.multiplyExact(fullTimeEmpHrs, empRatePerHr);
+                        System.out.println("Employee Daily Wage is :" + empWage);
+                        totalWorkingHrs = totalWorkingHrs + 8;
+                        totalWorkingDays = totalWorkingDays++;
+                        break;
+                    case 2:
+                        System.out.println("Employee is Present but Half-Time");
+                        empWage = Math.multiplyExact(partTimeEmpHr, empRatePerHr);
+                        System.out.println("Employee's Part-Time Wage is :" + empWage);
+                        totalWorkingHrs = totalWorkingHrs + 4;
+                        totalWorkingDays = totalWorkingDays++;
+                        break;
+                }
+                totalMonthlyWage = totalMonthlyWage + empWage;
+            }
+
+            System.out.println("Company name: "+company);
+            System.out.println("Employee Monthly Wage is :" + totalMonthlyWage);
+
+        }
+
+        /**
+         * Here this method will take the parameters as input.
+         */
+        public static void main(String [] args) {
+            System.out.println("Welcome To Employee wage Computation Program");
+            calculateTotalwage("DMart", 20, 20, 100);
+            calculateTotalwage("Reliance", 30, 15, 150);
+        }
+
+    }
+
+
+
+
+
+
+
